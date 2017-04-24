5e4cca966c8ec65345849e151fcebdc283b8c75a
Dini Nadzaruddin <dini.nurdayana@gmail.com>
2017-04-24 16:30:07 -0700

    assignment 2

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var http = require("http"), fs = require('fs');
+'use strict'
+
+var http = require("http"), fs = require('fs'), qs = require("querystring");
+let book = require("/public/book.js");
 
 function serveStatic(res, path, contentType, responseCode){
   if(!responseCode) responseCode = 200;
@@ -14,19 +17,31 @@ function serveStatic(res, path, contentType, responseCode){
   });
 }
 
-http.createServer(function(req,res){
-  var path = req.url.toLowerCase();
-  switch(path) {
-    case '/': 
-      serveStatic(res, '/public/home.html', 'text/html');
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('My project will be an app that tells you when is the next sunny day in Seattle');
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('404:Page not found.');
-  }
-  
+http.createServer((req,res) => {
+    let url = req.url.split("?");
+    let params = qs.parse(url[1]);
+    let path = url[0].toLowerCase();
+    
+    switch(path) {
+        case '/':
+            serveStatic(res, '/public/home.html', 'text/html');
+            break;
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('About the project');
+            break;
+        case '/get':
+            let found = book.get(params.title);
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            let results = (found) ? JSON.stringify(found) : "Not found";
+            res.end('Your results for ' + params.title + "/n" + results);
+            break;
+        case '/delete':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('delete');
+            break; 
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('404:Page not found.');
+    }
 }).listen(process.env.PORT || 3000);
