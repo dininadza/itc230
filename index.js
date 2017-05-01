2d5c2827e68b80e656d2727b2d47d9de038583f2
Dini Nadzaruddin <dini.nurdayana@gmail.com>
2017-04-30 18:22:51 -0700

    assignment 2 update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var http = require("http"), fs = require('fs'), qs = require("querystring");
-let book = require("/public/book.js");
+let book = require("./lib/book.js");
 
 function serveStatic(res, path, contentType, responseCode){
   if(!responseCode) responseCode = 200;
@@ -20,25 +20,28 @@ function serveStatic(res, path, contentType, responseCode){
 http.createServer((req,res) => {
     let url = req.url.split("?");
     let params = qs.parse(url[1]);
+    let found = book.get(params.title);
     let path = url[0].toLowerCase();
     
     switch(path) {
         case '/':
-            serveStatic(res, '/public/home.html', 'text/html');
+            serveStatic(res, '/lib/home.html', 'text/html');
             break;
         case '/about':
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end('About the project');
             break;
         case '/get':
-            let found = book.get(params.title);
+            console.log(book.get(params.title));
             res.writeHead(200, {'Content-Type': 'text/plain'});
-            let results = (found) ? JSON.stringify(found) : "Not found";
-            res.end('Your results for ' + params.title + "/n" + results);
+           // let results = (found) ? JSON.stringify(found) : " not found";
+            res.end('Your results for ' + params.title + ':  ' + JSON.stringify(found));
             break;
         case '/delete':
+            var deletethis = book.delete (params.title); 
+            console.log(book.delete(params.title))
             res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('delete');
+            res.end('selected book deleted: ' + params.title + '\n' + 'Total books remaining:' +  deletethis.Total);
             break; 
         default:
             res.writeHead(404, {'Content-Type': 'text/plain'});
